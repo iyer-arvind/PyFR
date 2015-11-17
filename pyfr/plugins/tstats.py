8026ad46924650c6db66d4b97e817b4763211121
Iyer Arvind Sundaram <a.iyer@imperial.ac.uk>
2015-11-17 14:02:14 +0000

    TStats Backup works

--- pyfr/plugins/tstats.py
+++ pyfr/plugins/tstats.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from collections import OrderedDict
+import os
+import shelve
 
 import numpy as np
 
@@ -33,26 +35,54 @@ class TStatsPlugin(BasePlugin):
                                 nvars=len(self.params))
 
         self.dt_out = self.cfg.getfloat(cfgsect, 'dt-out')
+        self.dt_bkp = self.cfg.getfloat(cfgsect, 'dt-bkp')
+
         self.nsteps = self.cfg.getfloat(cfgsect, 'nsteps')
 
-        self.tout_next = intg.tcurr + self.dt_out
+        self.tbkp_next = intg.tcurr + self.dt_bkp
+        self.nbkp = 0
 
         intg.call_plugin_dt(self.dt_out)
-
-        self.avg_start_t = intg.tcurr
-        self.prev_t = intg.tcurr
-
         plocs = intg.system.ele_ploc_upts
+
+        self.prev_t = intg.tcurr
         self.prev_soln = [self._process(s, p, intg)
                           for s, p in zip(intg.soln, plocs)]
 
-        self.accm_soln = [np.zeros_like(s) for s in self.prev_soln]
+        order = self.cfg.getint('solver', 'order')
+        prank = intg.rallocs.prank
+        self.bkp_base = '__bkp/accm-{}-r{}-p{}'.format(
+            intg.mesh_uuid, order, prank)
+        if not os.path.exists('__bkp'):
+            os.makedirs('__bkp')
+
+        bkp0 = shelve.open('{}-{}'.format(self.bkp_base, 0))
+        bkp1 = shelve.open('{}-{}'.format(self.bkp_base, 1))
+
+        if 'prev_t' in bkp0 and abs(bkp0['prev_t']-self.prev_t) < intg.dtmin:
+            self.avg_start_t = bkp0['avg_start_t']
+            self.accm_soln = bkp0['accm_soln']
+
+        elif 'prev_t' in bkp1 and abs(bkp1['prev_t']-self.prev_t) < intg.dtmin:
+            self.avg_start_t = bkp1['avg_start_t']
+            self.accm_soln = bkp1['accm_soln']
+
+        else:
+            self.avg_start_t = intg.tcurr
+            self.accm_soln = [np.zeros_like(s) for s in self.prev_soln]
+
+        bkp0.close()
+        bkp1.close()
+
+        self.tout_next = self.avg_start_t + self.dt_out
 
     def __call__(self, intg):
         time_to_write = abs(self.tout_next - intg.tcurr) < intg.dtmin
+        time_to_bckup = abs(self.tbkp_next - intg.tcurr) < intg.dtmin
 
         # Accumulate every nsteps and every time the file needs to be written
-        if (intg.nacptsteps % self.nsteps == 0) or time_to_write:
+        if (intg.nacptsteps % self.nsteps == 0) or time_to_write or \
+                time_to_bckup:
             dt_prev = intg.tcurr - self.prev_t
 
             plocs = intg.system.ele_ploc_upts
@@ -87,6 +117,19 @@ class TStatsPlugin(BasePlugin):
             self.accm_soln = [np.zeros_like(s) for s in self.prev_soln]
 
             self.tout_next += self.dt_out
+
+        if time_to_write or time_to_bckup:
+            bkup = shelve.open('{}-{}'.format(self.bkp_base, self.nbkp))
+            bkup.clear()
+
+            bkup['prev_t'] = self.prev_t
+            bkup['avg_start_t'] = self.avg_start_t
+            bkup['accm_soln'] = self.accm_soln
+
+            bkup.close()
+
+            self.nbkp ^= 1
+            self.tbkp_next += self.dt_bkp
 
     def _process(self, soln, plocs, intg):
         # Constants
