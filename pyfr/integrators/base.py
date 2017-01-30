70e609797f54bbe1af212b41240d80fbd0dec2e3
Iyer Arvind Sundaram <a.iyer@imperial.ac.uk>
2017-01-30 10:30:26 +0000

    Added print statements.

--- pyfr/integrators/base.py
+++ pyfr/integrators/base.py
@@ -195,8 +195,14 @@ class BaseIntegrator(object, metaclass=ABCMeta):
         pass
 
     def run(self):
+        t0 = time.time()
+        print('Start time: {}'.format(t0))
+
         for t in self.tlist:
             self.advance_to(t)
+
+        print('nfevals: {}'.format(self._stepper_nfevals))
+        print('Delta time: {}'.format(time.time() - t0))
 
     @property
     def nsteps(self):
